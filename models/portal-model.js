a5054b7fe03b250d6ecb1203fae1a0c5aa66eec0
Chad David Cover <chaddcover@gmail.com>
2018-02-11 14:59:56 -0500

    removed extraneous semicolons from portal-model.js; changed 'start' command in package.json

--- models/portal-model.js
+++ models/portal-model.js
@@ -20,15 +20,15 @@ const Project = module.exports = mongo.model('Project',ProjectPortalSchema);
 // getAllProjects() is ues to return all projects from MongoDB
 module.exports.getAllProjects = (callback) => {
    Project.find(callback);
-};
+}
 
 // addProject() is used to insert new project into MongoDB
 module.exports.addProject = (newProject,callback) => {
    newProject.save(callback);
-};
+}
 
 // deleteById() uses an id parameter to remove a project from MongoDB
 module.exports.deleteProjectById = (id,callback) => {
    let query = {_id: id};
    Project.remove(query,callback);
-};
+}
