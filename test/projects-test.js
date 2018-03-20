48e6803e63fe46072beda399908c16580d5a320b
kbagri <kanishq@gmail.com>
2018-03-20 16:43:23 -0400

    Added 5 new test cases for Add/View/Delete Projects. The 2nd delete Project test case currently fails 'coz of a defect. --DELETE project       1) Delete a project that doesnt exist. There is an open Defect for this

--- test/projects-test.js
+++ test/projects-test.js
@@ -11,6 +11,7 @@ let server = require('../app');
 let should = chai.should();
 
 chai.use(chaiHttp);
+var expect = chai.expect;
 
 // Projects parent block
 describe('Project Tests', () => {
@@ -48,6 +49,83 @@ describe('Project Tests', () => {
         });
     });
 
+
+    //Create a New Project with ALL the fields populated:
+    describe('POST project', () => {
+        it('it should POST a new project with title, description, ', (done) => {
+            let project = {
+                projectName: "Projects Portal",
+                projectDescription: "A portal app to manage software development projects",
+                projectMembers: "",
+                techStack: "Mongo,Express,Node,Angular",
+                repositoryLink: "www.github.com",
+                projectDemo: "www.youtube.com/project",
+                labels: "#Portal, #MEAN",
+            }
+            chai.request(server)
+            .post('/add')
+            .send(project)
+            .end((error,response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                response.body.should.have
+                  .property("message")
+                    .and.to.be.eql("Project added successfully.");
+                done();
+            });
+        });
+    });
+
+     //Create a New Project with project name Containing special Characters:
+     describe('POST project', () => {
+        it('it should POST a new project with title, description, ', (done) => {
+            let project = {
+                projectName: "$@mplE Pro73CT ~!@#$%^&",
+                projectDescription: "Project with special Characters and Spaces",
+                projectMembers: "",
+                techStack: "Mongo,Express,Node,Angular",
+                repositoryLink: "www.github.com",
+                projectDemo: "www.youtube.com/project",
+                labels: "#Portal, #MEAN",
+            }
+            chai.request(server)
+            .post('/add')
+            .send(project)
+            .end((error,response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                response.body.should.have
+                  .property("message")
+                    .and.to.be.eql("Project added successfully.");
+                done();
+            });
+        });
+    });
+
+     //Create a New Project with ProjectName Missing:
+     describe('POST project', () => {
+        it('it should FAIL trying to POST a new project with missing Project Name', (done) => {
+            let project = {
+                projectDescription: "A portal app to manage software development projects",
+                projectMembers: "",
+                techStack: "Mongo,Express,Node,Angular",
+                repositoryLink: "www.github.com",
+                projectDemo: "www.youtube.com/project",
+                labels: "#Portal, #MEAN",
+            }
+            chai.request(server)
+            .post('/add')
+            .send(project)
+            .end((error,response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                expect(response.body.message).to.include("`projectName` is required");
+                expect(response.body.success).to.be.false;
+                done();
+            });
+        });
+    });
+    
     /************************************/
     // TEST GET All Projects
     /************************************/
@@ -58,19 +136,38 @@ describe('Project Tests', () => {
             .end((error,response) => {
                 response.should.have.status(200);
                 response.should.be.a('object');
-                response.body.length.should.be.eql(1);
-                response.body[0].should.have.property('projectName')
-                    .and.to.eql('Projects Portal');
-                response.body[0].should.have
-                  .property("projectDescription")
-                  .and.to.eql(
-                    "A portal app to manage software development projects"
-                  );
+                  ;
                 done();
             });
         });
     });
 
+     //Test case summary: Get a Project by it's Id
+
+     describe('GET project', () => {
+        it('it should GET single Project by its ID', (done) => {
+            var id=null
+            chai.request(server)
+            .get('/view')
+            .end(function(err, res) 
+            {
+                chai.request(server)
+                .get('/view/'+ res.body[0]._id)
+                .end((error,response) => 
+                {
+                    response.should.have.status(200);
+                    response.should.be.a('object');
+                    response.body.length.should.be.eql(1);
+                    response.body[0].should.have.property('projectName')
+                        .and.to.eql('Projects Portal');
+                    response.body[0].should.have
+                      .property("projectDescription")
+                      .and.to.eql("A portal app to manage software development projects");
+                done();
+                });                
+            });
+    });
+})
     /************************************/
     // TEST DELETE route
     /************************************/
@@ -95,6 +192,28 @@ describe('Project Tests', () => {
     });
    });
 
+ //Test case id:
+   //Test Case summary: Delete a project that doesn't exist
+   //Test Case type:
 
+   describe('DELETE project', () => {
+    it('Delete a project that doesnt exist', (done) => {
+        let project = new Project({ 
+            projectName: "Yadda Yadda Yadda", 
+            projectDescription: "An app devoted to Bosco" 
+        });
+        project.save((error,project) => {
+            chai.request(server)
+            .delete('/delete/' + project.id)
+            .end((error, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                expect(response.body.message).to.include("Project does not Exist");
+                expect(response.body.success).to.not.be.false;
+                done();
+            });
+        });
+    });
+   });
 
 });
