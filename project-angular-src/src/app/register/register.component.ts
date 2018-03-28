a083fd5f6e025c78f5ed48c332442c903c3d6c2b
CodeDNA <parkerpunj@gmail.com>
2018-03-28 18:33:33 -0400

    Added Create account Page| Login Page Enhancement |Added BU Project Tacker Icon

--- project-angular-src/src/app/register/register.component.ts
+++ project-angular-src/src/app/register/register.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
 import { User } from '../models/User';
 import { UserSessionService } from '../services/user-session.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, EmailValidator } from '@angular/forms';
 import { ViewChild } from '@angular/core/src/metadata/di';
 
 
@@ -15,86 +14,95 @@ import { ViewChild } from '@angular/core/src/metadata/di';
 
 export class RegisterComponent implements OnInit {
   private newUser: User;
-  
+
 
   status = "";
+  lengthError = true;
+  private x = ""; y = "";
+  private First = "";
+  private Last = "";
+  private Email = "";
+  private allCool = false;
+  private confirmation = "";
+  errorStatus = false;
+  errorMessage = "";
+  success = false;
+  validForm = 0;
+  passLength = 0;
+  isEmailValid = 0;
 
- private x = "";
- private y = "";
- private allCool = false;
- private confirmation = "";
- errorStatus=false;
- errorMessage="";
- success=false;
- 
- 
- titles = [
-  {value: 'Mr.', viewValue: 'Mr.'},
-  {value: 'Ms.', viewValue: 'Ms.'},
-  {value: 'Mrs.', viewValue: 'Mrs.'}
-];
+
+  titles = [
+    { value: 'Mr.', viewValue: 'Mr.' },
+    { value: 'Ms.', viewValue: 'Ms.' },
+    { value: 'Mrs.', viewValue: 'Mrs.' }
+  ];
 
   constructor(private userSessionService: UserSessionService) { }
   @Output() userAdded: EventEmitter<User> = new EventEmitter<User>();
   ngOnInit() {
-    this.allCool = false;
-    this.errorStatus=false;
+    this.pageRefresh();
     this.newUser = {
-      _id:0,
+      _id: 0,
       firstName: '',
-      lastName:'',
-      title:'',
+      lastName: '',
+      title: '',
       email: '',
       password: '',
-      __v:0,
-      }; 
-      
+      __v: 0,
 
-} //page Initialization ends here
+    };
 
 
+  } //page Initialization ends here
+
+  pageRefresh() {
+    this.validForm = 0;
+    this.allCool = false;
+    this.errorStatus = false;
+    this.x = "";
+    this.y = "";
+    this.First = "";
+    this.Last = "";
+    this.Email = "";
+    this.passLength = 0;
+    this.isEmailValid = 0;
+    this.lengthError = false;
+  }
   //create new account
-onCreateNewAccount() {
-
-  if(this.allCool!=true){
-    console.log(this.newUser);
-      //---------------------------
+  onCreateNewAccount() {
+    this.validForm = this.formValidation();
+    if (this.allCool != true && this.validForm == 1) {
+      console.log(this.newUser);
       this.userSessionService.addUser(this.newUser).subscribe(
         response => {
           console.log(response);
           this.userAdded.emit(this.newUser);
-      //--------------------------
-          this.ngOnInit();
-          this.success=true;
-      });
+          this.success = true;
+          this.errorMessage = "User Added Successfully";
+          setTimeout(
+            () => {
+              this.success = false;
+            }, 4000);
+
+        });
+      this.ngOnInit();
+    }
+
+    else {
+      this.errorMessage = "Please check the form";
+      this.errorStatus = true;
+    }
+
 
   }
 
-  else
-  {
-    this.newUser = {
-      _id:0,
-      firstName: 'name',
-      lastName:'lname',
-      title:'tit',
-      email: 'email',
-      password:'pass',
-      __v:0,
-      }; 
-      
-    this.errorMessage="Please Check the form";
-    this.errorStatus=true;
-  }
-
-     
-}
- 
-  
-
-
+  //----------GET START-------------------------------------------
   getPassword1(event: Event) {
     this.x = (<HTMLInputElement>event.target).value;
     this.matchPasswords();
+    if (this.x.length < 8) this.lengthError = true;
+    else this.lengthError = false;
   }
 
   getPassword2(event: Event) {
@@ -102,18 +110,57 @@ onCreateNewAccount() {
     this.matchPasswords();
   }
 
+
+  
+  getFName(event: Event) {
+
+    this.First = (<HTMLInputElement>event.target).value;
+  }
+  getLName(event: Event) {
+    this.Last = (<HTMLInputElement>event.target).value;
+  }
+  getEmail(event: Event) {
+    this.Email = (<HTMLInputElement>event.target).value;
+  }
+//--------------GET END-------------------------------------------
+
+
+
   matchPasswords() {
 
     if (this.y == this.x && this.y != "" && this.x != "") {
-      this.confirmation = "";      
-      this.allCool=false;
+      this.confirmation = "";
+      this.allCool = false;
     }
     else
       if (this.y != "") {
         this.confirmation = "Passwords do not match ";
         this.allCool = true;
       }
-
   }//matchpasword_End
 
-}//class end
+
+  //--FORM VALIDATE---
+  formValidation() {
+    this.passLength = this.y.length;
+
+    if (this.First != "" && this.Last != "" && this.Email != ""
+      && this.passLength > 7 && this.emailvalidation() == 1) {
+      return 1;
+    }
+    else
+      return 0;
+  }
+
+  emailvalidation() {
+    var atI = this.Email.indexOf("@");
+    var dotI = this.Email.indexOf(".");
+    if (dotI != atI + 1
+      && atI != 0 && atI != -1
+      && dotI != -1) {
+      return 1;
+    }
+
+
+  }
+} 
