be5ca1bfc13176c34ef4fe47552d31a787c4472e
Andy <andrewoconnell89@gmail.com>
2018-04-25 23:01:30 -0400

    hardened Certificates

--- certificates/2--Create_Selfsigned_Cert.sh
+++ certificates/2--Create_Selfsigned_Cert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo openssl req -new -sha256 -nodes -out server.csr -newkey rsa:4096 -keyout server.key -config server.csr.cnf
+sudo openssl req -new -sha512 -nodes -out server.csr -newkey rsa:8192 -keyout server.key -config server.csr.cnf
 
-sudo openssl x509 -req -in server.csr -CA ~/ssl/rootCA.pem -CAkey ~/ssl/rootCA.key -CAcreateserial -out server.crt -days 365 -sha256 -extfile v3.ext
+sudo openssl x509 -req -in server.csr -CA ~/ssl/rootCA.pem -CAkey ~/ssl/rootCA.key -CAcreateserial -out server.crt -days 60 -sha512 -extfile v3.ext
