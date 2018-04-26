8e3c882a983df50456a9263d5357d9b52816b7ab
Chad David Cover <chad.cover@cengage.com>
2018-04-26 08:41:06 -0400

    removing project search component

--- project-angular-src/src/app/app-routing.module.ts
+++ project-angular-src/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ViewProjectComponent } from './view-project/view-project.component';
 import { AddProjectComponent } from './add-project/add-project.component';
-import { ProjectSearchComponent } from './project-search/project-search.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -14,7 +13,6 @@ import { DisplayProjectComponent } from './display-project/display-project.compo
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'projectSearch', component: ProjectSearchComponent},
   { path: 'home', component: HomeComponent},
   { path: 'addProject', component: AddProjectComponent},
   { path: 'profile', component: ProfileComponent},
